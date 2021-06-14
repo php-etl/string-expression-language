49e6608a69578de75d09fb54e58c2f1aead3e96d
sebprt <sebastien.parrat@orange.fr>
2021-06-14 10:26:07 +0200

    Initial commit

---
+++ src/StringExpressionLanguageProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kiboko\Component\StringExpressionLanguage;
+
+use Symfony\Component\ExpressionLanguage\ExpressionFunction;
+use Symfony\Component\ExpressionLanguage\ExpressionFunctionProviderInterface;
+
+class StringExpressionLanguageProvider implements ExpressionFunctionProviderInterface
+{
+    public function getFunctions(): array
+    {
+        return [
+            ExpressionFunction::fromPhp('sprintf', 'format')
+        ];
+    }
+}
