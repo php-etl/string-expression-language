28ce550be9fd39fec913d3576074df299431e3af
sebprt <sebastien.parrat@orange.fr>
2021-06-17 17:11:41 +0200

    Added phpunit with tests, and updated dependencies

---
+++ tests/functional/StringExpressionLanguageProviderTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace functional\Kiboko\Component\StringExpressionLanguage;
+
+use Kiboko\Component\StringExpressionLanguage\StringExpressionLanguageProvider;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+
+class StringExpressionLanguageProviderTest extends TestCase
+{
+    public function testFormatExpression(): void
+    {
+        $interpreter = new ExpressionLanguage(null, [new StringExpressionLanguageProvider()]);
+
+        $this->assertEquals('SKU_000001', $interpreter->evaluate('format("SKU_%06d", 1)'));
+    }
+}
