d8711f6c7c412e2514640eac354f7fa361283cc0
Jonathan <jonathan@kiboko.fr>
2024-11-07 16:52:35 +0100

    add preg_match expression

--- src/StringExpressionLanguageProvider.php
+++ src/StringExpressionLanguageProvider.php
@@ -24,6 +24,7 @@ class StringExpressionLanguageProvider implements ExpressionFunctionProviderInte
             ExpressionFunction::fromPhp('strip_tags', 'stripHtml'),
             ExpressionFunction::fromPhp('json_decode', 'decode'),
             ExpressionFunction::fromPhp('preg_replace', 'replaceByExpression'),
+            ExpressionFunction::fromPhp('preg_match', 'matchExpression'),
             new CapitalizeWords('capitalizeWords'),
             ExpressionFunction::fromPhp('rtrim', 'removeWhitespaces'),
             ExpressionFunction::fromPhp('explode', 'splitIntoArray'),
