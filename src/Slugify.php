cf8e2fbf03f94a3f2f42c39dd28ee81fe3122378
GitHub Action <actions@github.com>
2024-11-08 13:30:04 +0000

    [rector] Rector fixes

--- src/Slugify.php
+++ src/Slugify.php
@@ -41,7 +41,7 @@ final class Slugify extends ExpressionFunction
     {
         $text = preg_replace('~[^\pL\d]+~u', $divider, $value);
         $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-        $text = preg_replace('~[^-\w]+~', '', $text);
+        $text = preg_replace('~[^\-\w]+~', '', $text);
         $text = trim($text, $divider);
         $text = preg_replace('~-+~', $divider, $text);
         $text = strtolower($text);
