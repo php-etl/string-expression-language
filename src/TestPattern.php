8e96312ebfa3208c352b2a37e9b7ee0e00a1438a
Jonathan <jonathan@kiboko.fr>
2024-11-12 11:10:56 +0100

    replace preg_match by a custom expression

--- src/TestPattern.php
+++ src/TestPattern.php
@@ -4,13 +4,20 @@ declare(strict_types=1);
 
 namespace Kiboko\Component\StringExpressionLanguage;
 
-class TestPattern
+use Symfony\Component\ExpressionLanguage\ExpressionFunction;
+
+class TestPattern extends ExpressionFunction
 {
     public function __construct($name)
     {
+        parent::__construct(
+            $name,
+            $this->compile(...)->bindTo($this),
+            $this->evaluate(...)->bindTo($this)
+        );
     }
 
-    private function compile(string $pattern, string $subject): bool
+    private function compile(string $pattern, string $subject): string
     {
         return <<<PHP
                 \$result = preg_match({$pattern}, {$subject});
